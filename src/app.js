38f91693ae641c30f2be9b2b887b79f785c2b83b
JuHwan <juhwan203@kakao.com>
2022-12-06 16:42:25 +0900

    아이콘 마이그레이션

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 // app.js
+
+// 아이콘 마이그레이션 데이터
+import { icons_origin, icons_fa } from './icon.js'
+
 const API_KEY = '179ceeb11c23a912fefd41421f453ea0';
 let city_name = 'seoul';
 let API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`
@@ -17,10 +21,20 @@ fetch(API_URL)
 
 function showWeather(data) {
   const desc = data.weather[0].main; // 날씨상태 설명
-  const weather_icon = data.weather[0].icon; // 아이콘
+  let weather_icon = data.weather[0].icon; // 아이콘
   const temp = parseInt(data.main.temp - 273.15); // 현재온도
   const name = data.name; // 도시명
   console.log(name, desc, weather_icon, temp);
+
+  // 날씨 아이콘 교체
+  for (let i = 0; i < icons_origin.length;i++) {
+    if(icons_origin[i] == weather_icon) {
+      weather_icon = icons_fa[i]
+      console.log('변경아이콘 = ', weather_icon)
+      break;            
+    }
+  }
+
 
   // UI 출력(DOM)
   const citynameEl = document.querySelector('.cityname');
@@ -29,7 +43,8 @@ function showWeather(data) {
   const descEl = document.querySelector('.desc');
 
   citynameEl.textContent = name;
-  iconEl.innerHTML = `<img src='http://openweathermap.org/img/wn/${weather_icon}@2x.png' alt='아이콘'/>`
+  // iconEl.innerHTML = `<img src='http://openweathermap.org/img/wn/${weather_icon}@2x.png' alt='아이콘'/>`
+  iconEl.innerHTML = weather_icon;
   tempEl.innerHTML = `${temp}&deg;`
   descEl.textContent = desc;
 }
---
+++ src/icon.js
@@ -0,0 +1,42 @@
+export const icons_origin = [
+  '01d',  // 주간
+  '02d',
+  '03d',
+  '04d',
+  '09d',
+  '10d',
+  '11d',
+  '13d',
+  '50d',
+  '01n',  // 야간
+  '02n',
+  '03n',
+  '04n',
+  '09n',
+  '10n',
+  '11n',
+  '13n',
+  '50n'
+]
+
+export const icons_fa = [
+  '<i class="fas fa-sun"></i>', // 해()
+  '<i class="fas fa-cloud-sun"></i>',  // 해구름
+  '<i class="fa-solid fa-cloud"></i>', // 구름
+  '<i class="fa-solid fa-cloud"></i>',  // 구름많음
+  '<i class="fas fa-cloud-showers-heavy"></i>', // 많은비
+  '<i class="fas fa-cloud-showers-heavy"></i>',  // 비
+  '<i class="fas fa-bolt"></i>', // 번개바람
+  '<i class="fas fa-snowflake"></i>', // 눈
+  '<i class="fas fa-smog"></i>',
+  // 밤
+  '<i class="fa-solid fa-moon-stars"></i>', // 달
+  '<i class="fa-solid fa-moon-cloud"></i>', // 달구름
+  '<i class="fa-solid fa-cloud"></i>', // 구름
+  '<i class="fa-solid fa-cloud"></i>',  // 구름많음
+  '<i class="fas fa-cloud-showers-heavy"></i>', // 많은비
+  '<i class="fas fa-cloud-showers-heavy"></i>', // 비
+  '<i class="fas fa-bolt"></i>', // 번개바람
+  '<i class="fas fa-snowflake"></i>', // 눈
+  '<i class="fas fa-smog"></i>',
+]
